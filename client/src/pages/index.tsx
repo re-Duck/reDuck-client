207b19a883b2fb26ec34513c45dbfdb2c2dd4b89
bae-sh <hyeon1293@gmail.com>
2023-04-03 21:19:06 +0900

    feat : router 기능 추가

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
 export default function Home() {
   return (
     <>
@@ -12,11 +13,35 @@ export default function Home() {
 
       <main>
         <nav className="w-full h-14 border-b-2 border-gray-100">
-          <ol className="m-auto p-8 max-w-6xl flex justify-between items-center h-full">
-            <div>reDcuk</div>
-            <div>목록</div>
-            <div>프로필</div>
-          </ol>
+          <ul className="m-auto p-8 max-w-6xl flex justify-between items-center h-full">
+            <li>
+              <Link href="/" className="text-2xl font-bold">
+                reDcuk
+              </Link>
+            </li>
+
+            <li className="flex-auto pl-8">
+              <ul className=" flex gap-8 text-gray-500">
+                <li>
+                  <Link href="/board">게시판</Link>
+                </li>
+                <li>
+                  <Link href="/qna">Q&A</Link>
+                </li>
+                <li>
+                  <Link href="/chatroom">채팅방</Link>
+                </li>
+              </ul>
+            </li>
+
+            <li className="w-32 font-bold">
+              <ul>
+                <li>
+                  <Link href="/login">로그인</Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
         </nav>
       </main>
     </>

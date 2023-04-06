58c4bf99cce3b2589a17d1daa9fe97e14c7faacb
bae-sh <hyeon1293@gmail.com>
2023-04-07 00:27:09 +0900

    chore : 햄버거 링크 위치 이동

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -4,7 +4,12 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />

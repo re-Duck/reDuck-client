e9843a403f84b253f4047f65b7616f54a35dd2e5
DongyounYim <dyyim4725@gmail.com>
2023-04-10 15:43:17 +0900

    [chore] 로그인 화면 디자인 완성

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { Icon } from "@iconify/react";
+import googleLogo from "../../assets/images/google_logo.png";
 
 export default function Login() {
   return (
@@ -8,7 +10,7 @@ export default function Login() {
       <Link href="/" className="text-2xl font-bold">
         reDuck🐥
       </Link>
-      <form className="flex flex-col shadow-lg p-8 gap-y-3 min-w-[24rem] max-w-xl">
+      <form className="flex flex-col shadow-lg p-8 m-4 gap-y-3 min-w-[22rem] max-w-xl">
         <label>ID</label>
         <input
           type="text"
@@ -22,23 +24,23 @@ export default function Login() {
           className="relative block w-full rounded-b-md border-0 p-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
         />
         <button className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-          Login
+          Sign in
         </button>
-        <hr />
-        <div className="flex justify-between">
-          <button className="flex gap-x-2 rounded-md bg-[#4285F4] px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-            <Icon icon="ion:logo-google" style={{ fontSize: "20px" }} />
-            <span>Google Login</span>
-          </button>
-          <button className="group relative flex gap-x-2 rounded-md bg-black px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-            <Icon icon="mdi:github" style={{ fontSize: "20px" }} />
-            <span>Github Login</span>
-          </button>
-        </div>
+        <hr className="border-0 border-t-2 text-center overflow-visible after:content-['OR'] after:relative after:top-[-17px] p-1 after:text-gray-500 after:bg-white" />
+        <button className="flex text-center gap-x-2 rounded-md shadow-md bg-white px-3 py-2 text-sm font-semibold text-gray hover:shadow-none focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          <Image src={googleLogo} alt="googleLogo" style={{ width: "20px" }} />
+          <span className="flex-grow">Sign in with Google</span>
+        </button>
+        <button className="group relative flex gap-x-2 rounded-md bg-black px-3 py-2 text-sm font-semibold text-white hover:bg-zinc-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          <Icon icon="mdi:github" style={{ fontSize: "20px" }} />
+          <span className="flex-grow">Sign in with Github</span>
+        </button>
         <div className="flex justify-evenly">
-          <span>Register</span>
+          <Link href="/signup" className="underline">
+            Register
+          </Link>
           <span>|</span>
-          <span>FindPW</span>
+          <span className="underline">FindPW</span>
         </div>
       </form>
     </div>

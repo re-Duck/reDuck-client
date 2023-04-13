a17564b5430873ea7a0604d77c2dc379beaefb62
bae-sh <hyeon1293@gmail.com>
2023-04-13 18:42:08 +0900

    feat : 댓글 UI 구현

--- client/src/components/Post/comment.tsx
+++ client/src/components/Post/comment.tsx
@@ -1,11 +1,15 @@
+import Image from 'next/image';
 import React from 'react';
 
-interface IPost {
-  id: string;
-}
-export default function Comment({ id }: IPost) {
+import googleLogo from '../../assets/images/google_logo.png';
+
+export default function Comment() {
   return (
     <div className="flex flex-col w-full max-w-4xl m-auto bg-white border-gray-100 border-2 p-6 gap-7">
+      <div className="flex gap-4">
+        <Image src={googleLogo} alt="googleLogo" style={{ width: '20px' }} />
+        <span className=" font-semibold">배성현</span>
+      </div>
       <p className="text-md text-gray-500">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias saepe odit eos laborum culpa,
         animi sapiente! Voluptas est rem ipsum ex. Aspernatur ab voluptatem saepe deleniti incidunt
--- client/src/pages/board/[id].tsx
+++ client/src/pages/board/[id].tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import { Layout, PostDetail, Comment } from '@/components';
+import Image from 'next/image';
+
+import googleLogo from '../../assets/images/google_logo.png';
 
 export default function PostDetailPage() {
   const router = useRouter();
@@ -11,16 +14,18 @@ export default function PostDetailPage() {
       <div className="flex flex-col max-w-4xl m-auto gap-14 mb-4">
         <PostDetail id={id} />
         <h3 className="text-2xl font-bold pl-3">댓글 6</h3>
-        <table className="flex flex-col gap-4  border-gray-100 border-2">
-          <tr className="flex justify-between items-center gap-1 h-16 bg-white border-gray-100 border-2 px-10">
-            <div>img</div>
+        <table className="flex flex-col border-gray-100 border-[1px] border-collapse">
+          <tr className="flex justify-between items-center gap-1 h-16 bg-white border-gray-100 border-[1px] px-10">
+            <Image src={googleLogo} alt="googleLogo" style={{ width: '20px' }} />
             <input
-              className=" border-b-gray-200 border-b-2 p-2 pl-3 w-9/12"
+              className=" border-b-gray-200 border-b-[1px] p-2 pl-3 w-9/12"
               placeholder="댓글을 입력해 보세요."
             />
             <button className=" bg-red-400 rounded-lg px-3 py-2 text-white text-xs">등록</button>
           </tr>
-          {/* <Comment id={id} /> */}
+          <tr>
+            <Comment />
+          </tr>
         </table>
       </div>
     </Layout>

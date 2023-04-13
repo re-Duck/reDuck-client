5e0a1d3cf96680ca28190e6b0ed9c45293f2447b
bae-sh <hyeon1293@gmail.com>
2023-04-13 18:44:35 +0900

    feat : 게시글 상세 UI 구현

--- client/src/pages/board/[id].tsx
+++ client/src/pages/board/[id].tsx
@@ -13,7 +13,7 @@ export default function PostDetailPage() {
     <Layout>
       <div className="flex flex-col max-w-4xl m-auto gap-14 mb-4">
         <PostDetail id={id} />
-        <h3 className="text-2xl font-bold pl-3">댓글 6</h3>
+        <h3 className="text-2xl font-bold pl-3">댓글 3</h3>
         <table className="flex flex-col border-gray-100 border-[1px] border-collapse">
           <tr className="flex justify-between items-center gap-1 h-16 bg-white border-gray-100 border-[1px] px-10">
             <Image src={googleLogo} alt="googleLogo" style={{ width: '20px' }} />
@@ -22,6 +22,12 @@ export default function PostDetailPage() {
               placeholder="댓글을 입력해 보세요."
             />
             <button className=" bg-red-400 rounded-lg px-3 py-2 text-white text-xs">등록</button>
+          </tr>
+          <tr>
+            <Comment />
+          </tr>
+          <tr>
+            <Comment />
           </tr>
           <tr>
             <Comment />

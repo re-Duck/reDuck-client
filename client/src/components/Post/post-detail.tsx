3e6de8cf460c3f76e40f4e42005054da90fc13b3
bae-sh <hyeon1293@gmail.com>
2023-04-13 17:57:37 +0900

    feat : 게시글 상세 UI 구현

--- client/src/components/Post/post-detail.tsx
+++ client/src/components/Post/post-detail.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 
 interface IPost {
-  id: string;
+  id: string | string[] | undefined;
 }
 export default function PostDetail({ id }: IPost) {
   return (
-    <article className="flex flex-col w-full  bg-white border-gray-100 border-2 h-50 p-6 hover:cursor-pointer">
+    <article className="flex flex-col w-full max-w-4xl m-auto bg-white border-gray-100 border-2 p-6 gap-7">
       <h1 className="text-xl font-bold mb-4">제목 {id}</h1>
-      <p className="text-md text-gray-500 ">
+      <p className="text-md text-gray-500">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias saepe odit eos laborum culpa,
         animi sapiente! Voluptas est rem ipsum ex. Aspernatur ab voluptatem saepe deleniti incidunt
         pariatur, modi iusto! Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias saepe
         odit eos laborum culpa, animi sapiente! Voluptas est rem ipsum ex. Aspernatur ab voluptatem
-        saepe deleniti incidunt pariatur, modi iusto!
+        saepe deleniti incidunt pariatur, modi iusto! Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Alias saepe odit eos laborum culpa, animi sapiente! Voluptas est rem ipsum
+        ex. Aspernatur ab voluptatem saepe deleniti incidunt pariatur, modi iusto! Lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Alias saepe odit eos laborum culpa, animi sapiente!
+        Voluptas est rem ipsum ex. Aspernatur ab voluptatem saepe deleniti incidunt pariatur, modi
+        iusto!
       </p>
+      <p className="text-gray-400">2023년 4월 13일</p>
+      <hr />
+      <p className="text-gray-400 text-sm">좋아요 261 | 조회 7,042</p>
     </article>
   );
 }
--- client/src/pages/board/[id].tsx
+++ client/src/pages/board/[id].tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Layout from '@/components/Layout';
 import { useRouter } from 'next/router';
+import { PostDetail } from '@/components';
 
 export default function PostDetailPage() {
   const router = useRouter();
   const { id } = router.query;
   return (
     <Layout>
-      <h2>상세페이지</h2>
-      <p>Post: {id}</p>
+      <PostDetail id={id} />
     </Layout>
   );
 }

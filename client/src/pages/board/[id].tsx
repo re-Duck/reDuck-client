4da64dc1578419fe11b7232c656540266ca9bff8
bae-sh <hyeon1293@gmail.com>
2023-04-13 19:07:39 +0900

    chore : table -> div 태그 수정

--- client/src/pages/board/[id].tsx
+++ client/src/pages/board/[id].tsx
@@ -13,26 +13,25 @@ export default function PostDetailPage() {
     <Layout>
       <div className="flex flex-col max-w-4xl m-auto gap-14 mb-4">
         <PostDetail id={id} />
+
         <h3 className="text-2xl font-bold pl-3">댓글 3</h3>
-        <table className="flex flex-col border-gray-100 border-[1px] border-collapse">
-          <tr className="flex justify-between items-center gap-1 h-16 bg-white border-gray-100 border-[1px] px-10">
+        <div className="flex flex-col border-gray-100 border-[1px] border-collapse">
+          <div className="flex justify-between items-center gap-1 h-16 bg-white border-gray-100 border-[1px] px-10">
             <Image src={googleLogo} alt="googleLogo" style={{ width: '20px' }} />
             <input
               className=" border-b-gray-200 border-b-[1px] p-2 pl-3 w-9/12"
               placeholder="댓글을 입력해 보세요."
             />
             <button className=" bg-red-400 rounded-lg px-3 py-2 text-white text-xs">등록</button>
-          </tr>
-          <tr>
+          </div>
+
+          <article>
             <Comment />
-          </tr>
-          <tr>
+          </article>
+          <article>
             <Comment />
-          </tr>
-          <tr>
-            <Comment />
-          </tr>
-        </table>
+          </article>
+        </div>
       </div>
     </Layout>
   );

4e4c509c19e278be7f2acd30837791f77fa57194
DongyounYim <dyyim4725@gmail.com>
2023-04-18 21:49:37 +0900

    [feat] Divider 컴포넌트 추가

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -3,3 +3,4 @@ export { default as Layout } from '@/components/Layout';
 export { default as MenuView } from '@/components/MenuView';
 export { default as Navigator } from '@/components/Navigator';
 export { Post, PostDetail, Comment } from '@/components/Post';
+export { default as Divider} from "@/components/Divider"

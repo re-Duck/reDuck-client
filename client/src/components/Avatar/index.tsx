426ad0dc51889ea536b4b062983f1dc9912a9daa
DongyounYim <dyyim4725@gmail.com>
2023-04-20 00:27:56 +0900

    [feat] Avatar 컴포넌트 추가

---
+++ client/src/components/Avatar/index.tsx
@@ -0,0 +1,30 @@
+import { positionStyle } from '@/styles/styleConstant';
+
+interface AvatarProp {
+  src: string | null;
+  alt: string;
+  hasDot: boolean;
+  position?: 'topRight' | 'bottomRight' | 'topLeft' | 'bottomLeft';
+}
+
+export default function Avatar({ src, alt, hasDot, position }: AvatarProp) {
+  const backgroundColor = src ? '' : 'bg-slate-400';
+  return (
+    <div
+      className={`relative rounded-full ${backgroundColor} border border-slate-200 w-[80px] h-[80px] max-w-[100px] max-h-[100px] overflow-hidden`}
+    >
+      {src ? (
+        <img src={src} alt={alt} className="w-full h-full object-cover" />
+      ) : (
+        <></>
+      )}
+      {hasDot && (
+        <div
+          className={`${
+            positionStyle[position!]
+          } absolute z-10 bg-white rounded-full p-4`}
+        />
+      )}
+    </div>
+  );
+}
--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -3,4 +3,5 @@ export { default as Layout } from '@/components/Layout';
 export { default as MenuView } from '@/components/MenuView';
 export { default as Navigator } from '@/components/Navigator';
 export { Post, PostDetail, Comment } from '@/components/Post';
-export { default as Divider} from "@/components/Divider"
+export { default as Divider } from '@/components/Divider';
+export { default as Avatar } from '@/components/Avatar';

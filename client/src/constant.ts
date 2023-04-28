2d4af6cf372499e17b0ec21e4f4e1d21390cc984
bae-sh <hyeon1293@gmail.com>
2023-04-28 18:12:04 +0900

    feat : Formik 설정 및 에러메세지 작성

--- client/src/constant.ts
+++ client/src/constant.ts
@@ -4,13 +4,17 @@ export const linkList = [
   { name: '채팅방', href: '/chatroom' },
 ];
 
-export const menuViewLinkList = [...linkList, { name: '로그인', href: '/login' }];
+export const menuViewLinkList = [
+  ...linkList,
+  { name: '로그인', href: '/login' },
+];
 
 export const initialLoginValue = { userId: '', password: '' };
 
 export const errorMessage = {
-  blankID: "아이디는 필수 입력값 입니다.",
-  blankPassword: "비밀번호는 필수 입력값 입니다.",
+  blankID: '아이디는 필수 입력값 입니다.',
+  blankPassword: '비밀번호는 필수 입력값 입니다.',
+  blankTitle: '제목은 필수 입력값 입니다.',
 };
 
 export const postList = [
--- client/src/pages/write/index.tsx
+++ client/src/pages/write/index.tsx
@@ -1,13 +1,23 @@
-import { QuillEditBox } from '@/components';
-import Layout from '@/components/Layout';
 import React from 'react';
+import Layout from '@/components/Layout';
+
+import { QuillEditBox } from '@/components';
+
+import { Formik, Field, Form } from 'formik';
+import * as Yup from 'yup';
+import { errorMessage } from '@/constant';
+
+const ValidationSchema = Yup.object().shape({
+  title: Yup.string().required(errorMessage.blankTitle),
+});
 
 export default function Write() {
   return (
     <Layout>
-      <div>
+      <Formik initialValues={{ title: '' }} validationSchema={ValidationSchema}>
+        <input type="text" className="" />
         <QuillEditBox />
-      </div>
+      </Formik>
     </Layout>
   );
 }

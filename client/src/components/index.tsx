362a340e9d6cb6ab046c100b3e1a59301833c88a
bae-sh <hyeon1293@gmail.com>
2023-05-02 12:15:48 +0900

    chore : component import 주소 수정

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -3,4 +3,4 @@ export { default as Layout } from '@/components/Layout';
 export { default as MenuView } from '@/components/MenuView';
 export { default as Navigator } from '@/components/Navigator';
 export { Post, PostDetail, Comment } from '@/components/Post';
-export { default as QuillEditBox } from './QuillEditBox/index';
+export { default as QuillEditBox } from '@/components/QuillEditBox';

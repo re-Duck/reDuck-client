554bd095b6daa629b9363f7cb9bb8ba13b988065
bae-sh <hyeon1293@gmail.com>
2023-05-02 12:02:12 +0900

    chore : 박스 그림자 제거

--- client/src/pages/write/index.tsx
+++ client/src/pages/write/index.tsx
@@ -31,46 +31,48 @@ export default function Write() {
   );
 
   return (
-    <div className="bg-gray-50">
-      <Formik
-        initialValues={{ title: '' }}
-        validationSchema={ValidationSchema}
-        onSubmit={async ({ title }, { setSubmitting }) =>
-          handleSubmit(title, setSubmitting)
-        }
-      >
-        {({ errors, touched, isSubmitting }) => (
-          <Form className="flex flex-col shadow-lg p-10 m-auto gap-y-5 max-w-5xl pb-20">
-            <div className="flex justify-between">
-              <Link href="/">
-                <Image
-                  src={arrow_back}
-                  alt="arrow_back"
-                  style={{ width: '30px' }}
-                />
-              </Link>
-              <button
-                className="rounded-md w-15 bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:opacity-70"
-                type="submit"
-                disabled={isSubmitting}
-                onClick={() => {
-                  errors.title && alert(errors.title);
-                }}
-              >
-                완료
-              </button>
-            </div>
-            <Field
-              name="title"
-              type="text"
-              placeholder="제목을 입력하세요"
-              className="text-4xl p-3 border-2 rounded-md border-gray-100 focus:outline-none focus:ring-2 focus:ring-slate-200 focus:border-transparent text-slate-700 bg-transparent"
-            />
+    <div className="bg-gray-50 h-screen">
+      {
+        <Formik
+          initialValues={{ title: '' }}
+          validationSchema={ValidationSchema}
+          onSubmit={async ({ title }, { setSubmitting }) =>
+            handleSubmit(title, setSubmitting)
+          }
+        >
+          {({ errors, touched, isSubmitting }) => (
+            <Form className="flex flex-col p-10 m-auto gap-y-5 max-w-5xl pb-20">
+              <div className="flex justify-between">
+                <Link href="/">
+                  <Image
+                    src={arrow_back}
+                    alt="arrow_back"
+                    style={{ width: '30px' }}
+                  />
+                </Link>
+                <button
+                  className="rounded-md w-15 bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:opacity-70"
+                  type="submit"
+                  disabled={isSubmitting}
+                  onClick={() => {
+                    errors.title && alert(errors.title);
+                  }}
+                >
+                  완료
+                </button>
+              </div>
+              <Field
+                name="title"
+                type="text"
+                placeholder="제목을 입력하세요"
+                className="text-4xl p-3 border-2 rounded-md border-gray-100 focus:outline-none focus:ring-2 focus:ring-slate-200 focus:border-transparent text-slate-700 bg-transparent"
+              />
 
-            <QuillEditBox content={content} handleContent={handleContent} />
-          </Form>
-        )}
-      </Formik>
+              <QuillEditBox content={content} handleContent={handleContent} />
+            </Form>
+          )}
+        </Formik>
+      }
     </div>
   );
 }

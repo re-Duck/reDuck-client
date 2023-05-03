2534f7c89f34ccb166b080ae30812c9f5b1b79cf
bae-sh <hyeon1293@gmail.com>
2023-05-03 19:49:03 +0900

    chore : 게시글 rounded 수정

--- client/src/components/Post/post.tsx
+++ client/src/components/Post/post.tsx
@@ -6,15 +6,16 @@ interface IPost {
 }
 export function Post({ id }: IPost) {
   return (
-    <article className="flex flex-col w-full  bg-white border-gray-100 border-2 h-50 p-6 hover:cursor-pointer">
+    <article className="flex flex-col w-full  bg-white border-gray-100 border-2 h-50 p-6 hover:cursor-pointer rounded-lg">
       <Link href={`/board/${id}`}>
         <h1 className="text-xl font-bold mb-4">제목</h1>
         <p className="text-md text-gray-500 line-clamp-3">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias saepe odit eos laborum
-          culpa, animi sapiente! Voluptas est rem ipsum ex. Aspernatur ab voluptatem saepe deleniti
-          incidunt pariatur, modi iusto! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Alias saepe odit eos laborum culpa, animi sapiente! Voluptas est rem ipsum ex. Aspernatur
-          ab voluptatem saepe deleniti incidunt pariatur, modi iusto!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias saepe
+          odit eos laborum culpa, animi sapiente! Voluptas est rem ipsum ex.
+          Aspernatur ab voluptatem saepe deleniti incidunt pariatur, modi iusto!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias saepe
+          odit eos laborum culpa, animi sapiente! Voluptas est rem ipsum ex.
+          Aspernatur ab voluptatem saepe deleniti incidunt pariatur, modi iusto!
         </p>
       </Link>
     </article>

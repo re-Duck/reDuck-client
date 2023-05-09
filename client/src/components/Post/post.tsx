03e3f171243db084b8d612327c90b054b9597d06
bae-sh <hyeon1293@gmail.com>
2023-05-09 23:54:08 +0900

    feat : props 데이터 연동 구현

--- client/src/components/Post/post.tsx
+++ client/src/components/Post/post.tsx
@@ -2,21 +2,31 @@ import Link from 'next/link';
 import React from 'react';
 
 interface IPost {
-  id: string;
+  comments: string | null;
+  postAuthorDevelopAnnual: string;
+  postAuthorId: string;
+  postAuthorName: string;
+  postAuthorProfileImg: {
+    uploadedFileName: string;
+    storagedFileName: string;
+    extension: string;
+    size: number;
+    path: string;
+  };
+  postContentPath: string;
+  postCreatedAt: string;
+  postOriginId: string;
+  postTitle: string;
+  postType: string;
+  postUpdatedAt: string;
 }
-export function Post({ id }: IPost) {
+
+export function Post(props: IPost) {
   return (
     <article className="flex flex-col w-full  bg-white border-gray-100 border-2 h-50 p-6 hover:cursor-pointer rounded-lg">
-      <Link href={`/board/${id}`}>
-        <h1 className="text-xl font-bold mb-4">제목</h1>
-        <p className="text-md text-gray-500 line-clamp-3">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias saepe
-          odit eos laborum culpa, animi sapiente! Voluptas est rem ipsum ex.
-          Aspernatur ab voluptatem saepe deleniti incidunt pariatur, modi iusto!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias saepe
-          odit eos laborum culpa, animi sapiente! Voluptas est rem ipsum ex.
-          Aspernatur ab voluptatem saepe deleniti incidunt pariatur, modi iusto!
-        </p>
+      <Link href={`/board/${props.postOriginId}`}>
+        <h1 className="text-xl font-bold mb-4">{props.postTitle}</h1>
+        <p className="text-md text-gray-500 line-clamp-3">{props.comments}</p>
       </Link>
     </article>
   );

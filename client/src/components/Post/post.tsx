afb05c4f213203d08be7f425742f43217418d412
bae-sh <hyeon1293@gmail.com>
2023-05-10 00:24:24 +0900

    chore : content Lorem 값 설정

--- client/src/components/Post/post.tsx
+++ client/src/components/Post/post.tsx
@@ -26,7 +26,12 @@ export function Post(props: IPost) {
     <article className="flex flex-col w-full  bg-white border-gray-100 border-2 h-50 p-6 hover:cursor-pointer rounded-lg">
       <Link href={`/board/${props.postOriginId}`}>
         <h1 className="text-xl font-bold mb-4">{props.postTitle}</h1>
-        <p className="text-md text-gray-500 line-clamp-3">{props.comments}</p>
+        <p className="text-md text-gray-500 line-clamp-3">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia nam rem
+          perferendis quisquam nulla itaque atque tempora esse praesentium ipsam
+          eaque provident nemo accusantium, deleniti minus? Mollitia facere
+          laudantium voluptatum?
+        </p>
       </Link>
     </article>
   );

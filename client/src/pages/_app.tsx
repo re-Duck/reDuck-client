6894b2bb0e967a028bc491b12860567771b5a01f
bae-sh <hyeon1293@gmail.com>
2023-05-09 22:18:19 +0900

    feat : react-query _app 컴포넌트 설정

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -4,6 +4,21 @@ import type { AppProps } from 'next/app';
 
 import 'react-quill/dist/quill.snow.css';
 
+//react-query
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = React.useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }

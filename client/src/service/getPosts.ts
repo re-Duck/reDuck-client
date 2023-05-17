b95b774ff832fd4ed50b787040eb9ba17557d883
bae-sh <hyeon1293@gmail.com>
2023-05-17 21:54:53 +0900

    chore : 불필요한 헤더 제거

--- client/src/service/getPosts.ts
+++ client/src/service/getPosts.ts
@@ -1,10 +1,6 @@
 import { axios_get } from './base/api';
 import { POSTS_INISIATE_VALUE } from '@/constant';
 
-const headers = {
-  Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJyZWR1Y2siLCJyb2xlcyI6W3sibmFtZSI6IlJPTEVfVVNFUiJ9XSwiaWF0IjoxNjg0MzIwMzQ0LCJleHAiOjE2ODQ0MDY3NDR9.FQYyrtw8bfaUYvELweDe776_UVzdsKJAykj4y1ujQVE`,
-};
-
 export const getAllPosts = async ({ pageParam = '' }) => {
   const params = { pageParam, postType: ['stack', 'qna'], page: 10 };
   const suburl = '/post';

26830fad3f020d4a390f25d2041978792e3cf1e0
bae-sh <hyeon1293@gmail.com>
2023-05-23 00:15:03 +0900

    feat : dom 접근 대신 dangerouslySetInnerHTML 사용

--- client/src/components/Post/post-detail.tsx
+++ client/src/components/Post/post-detail.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 //interface
 import { IPostInformation } from '@/types';
@@ -9,17 +9,13 @@ interface PostDetail {
 }
 
 export default function PostDetail({ data }: PostDetail) {
-  const contentRef = React.useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    if (contentRef.current) {
-      contentRef.current.innerHTML = data.postContent;
-    }
-  }, []);
-
   return (
     <article className="flex flex-col min-w-full max-w-4xl m-auto bg-white border-gray-100 border-2 p-6 gap-7">
       <h1 className="text-xl font-bold mb-4">{data.postTitle}</h1>
-      <p className="text-md text-gray-500" ref={contentRef}></p>
+      <p
+        className="text-md text-gray-500"
+        dangerouslySetInnerHTML={{ __html: data.postContent }}
+      />
       <p className="text-gray-400">{parseDate(data.postCreatedAt)}</p>
       <hr />
       <p className="text-gray-400 text-sm">좋아요 0 | 조회 0</p>

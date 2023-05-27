65aee399aa3f3d693ad539442facc07067df2a01
DongyounYim <dyyim4725@gmail.com>
2023-05-27 21:34:39 +0900

    [Add] 유저 정보 불러오기

--- client/src/pages/profile/[id].tsx
+++ client/src/pages/profile/[id].tsx
@@ -7,9 +7,9 @@ import { Layout, Avatar, Divider } from '@/components';
 import { sideBarList } from '@/constant';
 import { signOut } from 'next-auth/react';
 
-import { BASE_URL } from '@/service/base/api';
+import { BASE_URL, axios_get } from '@/service/base/api';
 
-export default function Profile() {
+export default function Profile({ pageProps }) {
   const router = useRouter();
   const authState = useSelector((state) => state.auth);
 
@@ -19,34 +19,73 @@ export default function Profile() {
     logOut();
     signOut({ redirect: true, callbackUrl: '/' });
   };
+
+  const { userData } = pageProps;
+  const {
+    company,
+    companyEmail,
+    companyEmailAuthentication,
+    developAnnual,
+    email,
+    name,
+    posts,
+    school,
+    schoolEmail,
+    schoolEmailAuthentication,
+    userId,
+    userProfileImgPath,
+  } = userData;
   //TODO: 반응형 디자인 고려하기 (모바일 디자인)
   return (
     <Layout>
       <div className="flex mx-auto max-w-5xl p-8 gap-x-16">
         <div className="flex-none text-center">
           <Avatar
-            src={`${BASE_URL}${authState.userProfileImgPath}`}
+            src={`${BASE_URL}${userProfileImgPath}`}
             alt="profileImg"
             size="lg"
           />
           <div className="mt-4">
-            <p>임동윤</p>
-            <p>@NAVER</p>
-            <p>충남대학교 컴퓨터공학과</p>
+            <p>{name}</p>
+            <p>@{company}</p>
+            <p>{school}</p>
           </div>
           <Divider type="horizental" margin={4} thin={2} />
           <div className="sm:block hidden">
-            {sideBarList.map((name, id) => (
+            {sideBarList.map((content, id) => (
               <div
                 key={id}
                 className=" bg-indigo-600 p-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:opacity-70"
               >
-                {name}
+                {content}
               </div>
             ))}
           </div>
         </div>
-        <div className="flex-1">
+        <div className="flex flex-col flex-1 border p-8 gap-2">
+          <div className="flex">
+            <span className="w-24 min-w-fit">아이디</span>
+            <span>{userId}</span>
+          </div>
+          <div className="flex">
+            <span className="w-24 min-w-fit">이메일</span>
+            <span>{email}</span>
+          </div>
+          <div className="flex">
+            <span className="w-24 min-w-fit">학교이메일</span>
+            <span>{schoolEmailAuthentication ? schoolEmail : 'none'}</span>
+          </div>
+          <div className="flex">
+            <span className="w-24 min-w-fit">회사이메일</span>
+            <span>{companyEmailAuthentication ? companyEmail : 'none'}</span>
+          </div>
+          <div className="flex">
+            <span className="w-24 min-w-fit">개발연차</span>
+            <span>{developAnnual}</span>
+          </div>
+          <button className="flex-none rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:opacity-70">
+            정보수정
+          </button>
           {isMyPage && (
             <button
               className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:opacity-70"
@@ -59,4 +98,20 @@ export default function Profile() {
       </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const suburl = `/user/${id}`;
+  const res = await axios_get({
+    suburl,
+  });
+  return {
+    props: {
+      userData: res.data,
+    },
+  };
 }

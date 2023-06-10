aa4b4da0d778a8d0b97bfcd582c61e58d071a586
bae-sh <hyeon1293@gmail.com>
2023-06-11 01:57:23 +0900

    chore : 메인 게시글 작성자 및 좋아요 추가

--- client/src/components/Post/post.tsx
+++ client/src/components/Post/post.tsx
@@ -3,16 +3,28 @@ import React from 'react';
 
 //Interface
 import { IPostInformation } from '@/types';
+import Avatar from '../Avatar';
+import { BASE_URL } from '@/service/base/api';
 
 export function Post(props: IPostInformation) {
+  const url = props.postAuthorId
+    ? `${BASE_URL}${props.postAuthorProfileImgPath}`
+    : '';
   return (
     <Link href={`/board/${props.postOriginId}`}>
-      <article className="flex flex-col w-full  bg-white border-gray-100 border-2 h-50 p-6 hover:cursor-pointer rounded-lg">
-        <h1 className="text-xl font-bold mb-4">{props.postTitle}</h1>
+      <article className="flex flex-col w-full  bg-white border-gray-100 border-2 h-50 p-6 hover:cursor-pointer rounded-l gap-3">
+        <h1 className="text-xl font-bold">{props.postTitle}</h1>
+        <div className="flex gap-2 font-semibold items-center">
+          <Avatar src={url} alt="user_icon" size="xs" />
+          <p>{props.postAuthorName}</p>
+        </div>
         <p
           className="text-md text-gray-500 line-clamp-3"
           dangerouslySetInnerHTML={{ __html: props.postContent }}
         />
+        <div className="flex justify-between items-center">
+          <p className="text-gray-400 text-sm">좋아요 0 | 조회 0</p>
+        </div>
       </article>
     </Link>
   );

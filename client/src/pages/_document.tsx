ce3fb42dbe06328c2546c36ea110c010402ba957
DongyounYim <dyyim4725@gmail.com>
2023-06-12 21:26:11 +0900

    [chore] 헤더태그 warning 제거

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -4,12 +4,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>reDuck</title>
-        <meta name="description" content="reDuck site" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/main-duck.png" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />

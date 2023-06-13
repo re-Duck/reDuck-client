2fc7b7f2217dc1bc0d1606f3473ea80055fa11b3
bae-sh <hyeon1293@gmail.com>
2023-06-13 22:36:46 +0900

    chore : 메인 게시글에서 댓글 수 추가

--- client/src/components/Post/post.tsx
+++ client/src/components/Post/post.tsx
@@ -11,6 +11,7 @@ export function Post(props: IPostInformation) {
   const url = props.postAuthorId
     ? `${BASE_URL}${props.postAuthorProfileImgPath}`
     : '';
+  console.log(props);
   return (
     <article className="flex flex-col w-full  bg-white border-gray-100 border-2 h-50 p-6  rounded-l gap-3">
       <div className="flex">
@@ -36,9 +37,12 @@ export function Post(props: IPostInformation) {
           className="text-md text-gray-500 line-clamp-3 mb-3"
           dangerouslySetInnerHTML={{ __html: props.postContent }}
         />
-        <div className="flex flex-col gap-1">
-          <p className="text-gray-400 text-sm">좋아요 0 | 조회 0</p>
-          <p className="text-gray-400">{parseDate(props?.postCreatedAt)}</p>
+        <div className="flex justify-between">
+          <div className="flex flex-col gap-1">
+            <p className="text-gray-400 text-sm">좋아요 0 | 조회 0</p>
+            <p className="text-gray-400">{parseDate(props?.postCreatedAt)}</p>
+          </div>
+          <p className="text-gray-400 text-sm">{`댓글 ${props.commentsCount}`}</p>
         </div>
       </Link>
     </article>

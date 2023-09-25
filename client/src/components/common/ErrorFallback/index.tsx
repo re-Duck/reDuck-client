99fb86fee449881a90b6eb3910840f41efac25b4
bae-sh <hyeon1293@gmail.com>
2023-09-26 00:18:44 +0900

    feat: fallback 컴포넌트 에러 구현

--- client/src/components/common/ErrorFallback/index.tsx
+++ client/src/components/common/ErrorFallback/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { errorMessage } from '@/constants/constant';
 
 function ErrorFallback() {
   return (
-    <div>
-      <h1>Something went wrong</h1>
+    <div className="flex flex-col items-center gap-2 pt-4">
+      <h1 className="text-2xl font-semibold">{errorMessage.networkError}</h1>
+      <p>{errorMessage.tryAgarin}</p>
     </div>
   );
 }

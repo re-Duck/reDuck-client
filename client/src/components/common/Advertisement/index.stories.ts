ed1eb220231981441f06705d0467d24f44448f57
bae-sh <hyeon1293@gmail.com>
2023-10-04 18:45:00 +0900

    feat: advertise 컴포넌트 스토리북 생성

---
+++ client/src/components/common/Advertisement/index.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Advertisement from './index';
+
+const meta: Meta<typeof Advertisement> = {
+  title: 'Components/Advertisement',
+  component: Advertisement,
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Advertisement>;
+
+export const Primary: Story = {
+  args: {},
+};

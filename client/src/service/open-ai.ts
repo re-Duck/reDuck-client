a6ec757718847515495c1d80fd099acb6c9da64a
bae-sh <hyeon1293@gmail.com>
2023-10-05 20:35:56 +0900

    chore: apiKey env 환경 체크

--- client/src/service/open-ai.ts
+++ client/src/service/open-ai.ts
@@ -1,7 +1,7 @@
 import OpenAI from 'openai';
 
 const openai = new OpenAI({
-  apiKey: process.env.NEXT_PUBLIC_OPEN_API,
+  apiKey: process.env.OPENAI_API_KEY,
   dangerouslyAllowBrowser: true,
 });
 

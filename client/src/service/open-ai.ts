46f64da17a37f6c9700e687032d3bb283a201261
bae-sh <hyeon1293@gmail.com>
2023-10-05 20:39:33 +0900

    chore: env 값 복구

--- client/src/service/open-ai.ts
+++ client/src/service/open-ai.ts
@@ -1,7 +1,7 @@
 import OpenAI from 'openai';
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_OPEN_API,
   dangerouslyAllowBrowser: true,
 });
 

d6c7f4c62b3b66684cff8e375469c2efbf46e35a
bae-sh <hyeon1293@gmail.com>
2023-10-06 14:37:40 +0900

    chore: storybook 경로 환경설정 추가

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/nextjs';
+import path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -14,6 +15,11 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: 'tag',
+  },
+  webpackFinal: async (config) => {
+    //@ts-ignore
+    config.resolve.alias['@'] = path.resolve(__dirname, '../src/');
+    return config;
   },
 };
 export default config;

eaddbc9443afd2bd3a0652e6de7df70b0ff95450
bae-sh <hyeon1293@gmail.com>
2023-10-11 15:02:50 +0900

    docs: Layout storybook 구현

---
+++ client/src/components/base/Layout/index.stories.tsx
@@ -0,0 +1,67 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Layout from './index';
+
+const mockData = {
+  data: {
+    user: {
+      id: 'reduck',
+      userProfileImgPath:
+        '/home/nuhgnod/develup/storage/profile/reduck/ef97d2eb-9e89-4dae-a0df-6e932666fae3.png',
+      token: 'token',
+    },
+  },
+};
+
+const meta: Meta<typeof Layout> = {
+  title: 'Components/base/Layout',
+  component: Layout,
+  tags: ['autodocs'],
+
+  parameters: {
+    layout: 'fullscreen',
+    componentSubtitle:
+      '모든 페이지에서 공통적으로 보여지는 레이아웃 컴포넌트입니다.',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Layout>;
+
+export const Primary: Story = {
+  args: {},
+  parameters: {
+    docs: {
+      description: {
+        story: '**비로그인 시** 보여지는 레이아웃 컴포넌트입니다.',
+      },
+    },
+  },
+};
+export const Logined: Story = {
+  args: {},
+  parameters: {
+    docs: {
+      description: {
+        story: '**로그인 시** 보여지는 레이아웃 컴포넌트입니다.',
+      },
+    },
+    nextAuthMock: {
+      session: {
+        ...mockData,
+      },
+    },
+  },
+};
+
+export const Mobile: Story = {
+  args: {},
+  parameters: {
+    viewport: { defaultViewport: 'mobile1' },
+    docs: {
+      description: {
+        story: '**모바일 화면**에서 보여지는 레이아웃 컴포넌트입니다.',
+      },
+    },
+  },
+};

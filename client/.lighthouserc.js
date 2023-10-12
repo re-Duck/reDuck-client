2dcf249793630f22dbd61df2a91f7e935902f865
bae-sh <hyeon1293@gmail.com>
2023-10-12 19:47:59 +0900

    chore: lighthouse rc 통과 기준 설정

--- client/.lighthouserc.js
+++ client/.lighthouserc.js
@@ -4,17 +4,22 @@ module.exports = {
       startServerCommand: 'npm run start',
       startServerReadyPattern: 'ready on',
       url: ['http://localhost:3000'],
-      numberOfRuns: 5,
+      numberOfRuns: 3,
     },
     upload: {
       target: 'temporary-public-storage',
     },
     assert: {
       assertions: {
-        // performance 카테고리 점수가 90점 미만이면 warning
         'categories:performance': ['warn', { minScore: 0.9 }],
-        // accessibility 가 100점 미만이면 error
-        'categories:accessibility': ['error', { minScore: 1 }],
+        'categories:accessibility': ['warn', { minScore: 0.9 }],
+        'categories:best-practices': ['warn', { minScore: 0.9 }],
+        'categories:seo': ['warn', { minScore: 0.9 }],
+
+        'categories:performance': ['error', { minScore: 0.8 }],
+        'categories:accessibility': ['error', { minScore: 0.8 }],
+        'categories:best-practices': ['error', { minScore: 0.8 }],
+        'categories:seo': ['error', { minScore: 0.8 }],
       },
     },
   },

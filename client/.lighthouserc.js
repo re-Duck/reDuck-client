1e9bf1496ffd902856a927cefeb8ac21e6880ab8
bae-sh <hyeon1293@gmail.com>
2023-11-11 15:44:36 +0900

    chore: lighthouse 기준 점수 수정

--- client/.lighthouserc.js
+++ client/.lighthouserc.js
@@ -15,7 +15,7 @@ module.exports = {
       assertions: {
         'categories:performance': ['warn', { minScore: 0.9 }],
 
-        'categories:performance': ['error', { minScore: 0.8 }],
+        'categories:performance': ['error', { minScore: 0.7 }],
         'categories:accessibility': ['error', { minScore: 0.9 }],
         'categories:best-practices': ['error', { minScore: 0.9 }],
         'categories:seo': ['error', { minScore: 0.9 }],

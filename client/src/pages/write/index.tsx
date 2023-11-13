a78ef7c6f2209335f19c3bdff752119ca477a379
DongyounYim <dyyim4725@gmail.com>
2023-11-13 22:16:09 +0900

    [fix] useCallback 의존성 버그 수정

--- client/src/pages/write/index.tsx
+++ client/src/pages/write/index.tsx
@@ -68,7 +68,7 @@ export default function Write() {
         openModal({ type: ModalType.ERROR, message: errorMessage.tryAgain });
       }
     },
-    [content]
+    [content, editor]
   );
 
   return (

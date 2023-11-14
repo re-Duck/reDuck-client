f949420aa463194e35fb7c4014d3b4258216717b
DongYounYim <dyyim4725@gmail.com>
2023-11-14 11:47:39 +0900

    [Add] 미들웨어에 로그인 라우팅도 추가 및 리다이렉트

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -3,11 +3,21 @@ import type { NextRequest } from 'next/server';
 
 export function middleware(req: NextRequest) {
   const token = req.cookies.get('token');
+  const pathName = req.nextUrl.pathname;
 
-  // 토큰이 없을 경우 /로 리다이렉트
-  if (!token) return NextResponse.redirect(new URL('/', req.url));
+  switch (pathName) {
+    case '/chat':
+      // 토큰이 없을 경우 /로 리다이렉트
+      if (!token) return NextResponse.redirect(new URL('/', req.url));
+      break;
+    case '/login':
+      if (token) return NextResponse.redirect(new URL('/', req.url));
+      break;
+    default:
+      return;
+  }
 }
 
 export const config = {
-  matcher: ['/chat'],
+  matcher: ['/chat', '/login'],
 };

6179908a670cdea1a2d9864fa60e67069aedcc0a
DongYounYim <dyyim4725@gmail.com>
2023-11-21 14:30:34 +0900

    [chore] decodeJWT 로직 util로 분리

--- client/src/components/common/AuthComponent/index.tsx
+++ client/src/components/common/AuthComponent/index.tsx
@@ -14,6 +14,9 @@ import { userManager } from '@/service/user';
 // constant
 import { ModalType, errorMessage } from '@/constants/constant';
 
+// utils
+import { decodeJWT } from '@/util';
+
 export default function AuthComponent({
   children,
 }: {
@@ -23,20 +26,6 @@ export default function AuthComponent({
   const dispatch = useDispatch();
   const { openModal, closeModal } = useModal();
 
-  function decodeJWT(token: string) {
-    const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    const jsonPayload = decodeURIComponent(
-      atob(base64)
-        .split('')
-        .map(function (c) {
-          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        })
-        .join('')
-    );
-
-    return JSON.parse(jsonPayload);
-  }
   const getUserData = async () => {
     // reload시 유저 전역데이터 세팅
     try {
--- client/src/util/index.ts
+++ client/src/util/index.ts
@@ -61,3 +61,16 @@ export const formatDateToString = (dateString: string) => {
     return `${month}월 ${day}일`;
   }
 };
+
+export const decodeJWT = (token: string) => {
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split('')
+      .map((c) => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
+      .join('')
+  );
+
+  return JSON.parse(jsonPayload);
+};

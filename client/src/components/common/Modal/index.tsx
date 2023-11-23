6ca41e30f0fcebc6777397d3347d91bc0b14e02a
bae-sh <hyeon1293@gmail.com>
2023-11-23 22:32:49 +0900

    chore: cancel 오타 추가 수정

--- client/src/components/common/Modal/index.tsx
+++ client/src/components/common/Modal/index.tsx
@@ -17,9 +17,9 @@ export default function Modal() {
         <div className="relative z-20">
           <div className="fixed inset-0 bg-gray-500 bg-opacity-75"></div>
           <div className="fixed inset-0 z-20 overflow-y-auto">
-            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-              <div className="relative transform w-full overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:max-w-lg delay-1000">
-                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+            <div className="flex items-end justify-center min-h-full p-4 text-center sm:items-center sm:p-0">
+              <div className="relative w-full overflow-hidden text-left transition-all delay-1000 transform bg-white rounded-lg shadow-xl sm:my-8 sm:max-w-lg">
+                <div className="px-4 pt-5 pb-4 bg-white sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
                     <ModalIcon type={type} />
                     <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
@@ -37,7 +37,7 @@ export default function Modal() {
                   </div>
                 </div>
 
-                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                <div className="px-4 py-3 bg-gray-50 sm:flex sm:flex-row-reverse sm:px-6">
                   {IS_CONFIRM_MODAL ? (
                     <>
                       <ModalButton
@@ -47,7 +47,7 @@ export default function Modal() {
                           callback && callback();
                         }}
                       />
-                      <ModalButton type="cancle" onClick={closeModal} />
+                      <ModalButton type="cancel" onClick={closeModal} />
                     </>
                   ) : (
                     <ModalButton

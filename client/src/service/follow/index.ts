0e549005e7dac7bd02184b3c4ddfd5f5f497e4c0
Im-younique <dyyim4725@gmail.com>
2024-01-11 14:06:54 +0900

    [feat] 팔로우 상태 체크 로직 추가

--- client/src/service/follow/index.ts
+++ client/src/service/follow/index.ts
@@ -2,10 +2,12 @@ import requestFollow from './requestFollow';
 import getFollower from './getFollower';
 import getFollowing from './getFollowing';
 import cancleFollow from './cancleFollow';
+import checkFollowStatus from './checkFollowStatus';
 
 export const followManager = {
   requestFollow,
   getFollower,
   getFollowing,
   cancleFollow,
+  checkFollowStatus,
 };

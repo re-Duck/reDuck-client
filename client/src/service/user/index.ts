fd0e6b6a33e206083bd0f7f4e55308d01f87d362
Im-younique <dyyim4725@gmail.com>
2024-01-14 14:01:48 +0900

    [chore] import 파일명 구분

--- client/src/service/user/index.ts
+++ client/src/service/user/index.ts
@@ -4,7 +4,7 @@ import updateUser from './updateUser';
 import deleteUser from './deleteUser';
 import checkDuplicateUser from './checkDuplicateUser';
 import loginUser from './loginUser';
-import logoutUser from './logoutUser';
+import logoutUser from './logOutUser';
 
 export const userManager = {
   createUser,

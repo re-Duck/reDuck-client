731f4b1669f468d7aa2663dbcf363203c908a095
Im-younique <dyyim4725@gmail.com>
2024-02-13 13:45:21 +0900

    [chore] 버튼 컴포넌트 색상 오타 수정

--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -14,11 +14,11 @@ export default function Button(props: ButtonProps) {
     yellow:
       'bg-yello-scale-400 text-black hover:active:bg-yello-scale-500 disabled:bg-yello-scale-100 disabled:text-gray-scale-700',
     blue_gray:
-      'bg-gray-500 text-white hover:active:bg-blue-gray-800 disabled:bg-blue-gray-100',
+      'bg-gray-500 text-white hover:active:bg-blue-gray-scale-800 disabled:bg-blue-gray-100',
     yellow_line:
-      'bg-yellow-50 border-[1px] border-yellow-500 text-black disabled:bg-gray-50 disabled:border-gray-800 disabled:text-gray-scale-600',
+      'bg-yello-scale-50 border border-yello-scale-500 text-black disabled:bg-gray-scale-50 disabled:border-gray-scale-800 disabled:text-gray-scale-600',
     blue_gray_line:
-      'bg-blue-gray-scale-100 border-[1px] border-gray-900 disabled:bg-gray-scale-50 disabled:border-gray-800 disabled:text-blue-gray-600',
+      'bg-blue-gray-scale-100 border border-gray-900 disabled:bg-gray-scale-50 disabled:border-gray-800 disabled:text-blue-gray-600',
   } as const;
 
   return (

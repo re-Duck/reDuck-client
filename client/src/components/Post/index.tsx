3d0b1f15e5c5244d49eb440518d81f95aaf55459
Im-younique <dyyim4725@gmail.com>
2024-02-13 15:13:47 +0900

    [chore] 게시글 수정 컴포넌트 삭제

--- client/src/components/Post/index.tsx
+++ client/src/components/Post/index.tsx
@@ -3,5 +3,4 @@ export { default as Comment } from './Comment';
 export { default as CommentUpload } from './CommentUpload';
 export { default as DeleteButton } from './DeleteButton';
 export { default as ModifyCommentButton } from './ModifyCommentButton';
-export { default as ModifyCotentButton } from './ModifyContentButton';
 export { default as MoreButton } from './MoreButton';

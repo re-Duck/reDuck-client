56c92cae5f4254aa06b00399d064773bbc4a10bf
Im-younique <dyyim4725@gmail.com>
2024-02-26 00:29:35 +0900

    [chore] follow버튼 401 에러 핸들링 제거

--- client/src/components/FollowButton/FollowButtonErrorFallback.tsx
+++ client/src/components/FollowButton/FollowButtonErrorFallback.tsx
@@ -29,29 +29,7 @@ export default function FollowButtonErrorFallback({
   error,
   resetErrorBoundary,
 }: IProp) {
-  const { status } = error;
-
   const dispatch = useDispatch();
-
-  if (status === 401) {
-    const handleClick = () => {
-      dispatch(
-        openAlert({
-          type: AlertType.INFO,
-          message: errorMessage.needLogin,
-        })
-      );
-    };
-    // 로그인하지 않은 유저
-    return (
-      <button
-        onClick={handleClick}
-        className="w-20 p-2 text-sm font-semibold text-white bg-indigo-600 rounded-md text-pretty text hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:opacity-70 sm:w-24 sm:text-base"
-      >
-        팔로우
-      </button>
-    );
-  }
 
   dispatch(
     openAlert({

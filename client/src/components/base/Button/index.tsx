3222db4c219e9232d5b826a6083300bd6578dbe0
Im-younique <dyyim4725@gmail.com>
2024-03-01 01:10:23 +0900

    [chore] 버튼 스타일 추가

--- client/src/components/base/Button/index.tsx
+++ client/src/components/base/Button/index.tsx
@@ -5,7 +5,9 @@ export type ButtonColorType =
   | 'yellow_line'
   | 'blue_gray'
   | 'blue_gray_line'
-  | 'blue_gray_line_disabled';
+  | 'blue_gray_line_disabled'
+  | 'red'
+  | 'red_line';
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   color: ButtonColorType;
 }
@@ -22,6 +24,9 @@ export default function Button(props: ButtonProps) {
       'bg-blue-gray-scale-100 border border-gray-900 disabled:bg-gray-scale-50 disabled:border-gray-800 disabled:text-blue-gray-600',
     blue_gray_line_disabled:
       'bg-gray-scale-50 border-[1px] border-gray-800 text-blue-gray-scale-600 opacity-40',
+    red: 'bg-red-scale-600 text-white hover:bg-red-scale-700 active:bg-red-scale-700 disable:bg-red-scale-200',
+    red_line:
+      'border border-red-scale-600 text-red-scale-600 hover:border-red-scale-700 hover:bg-red-scale-200 active:border-red-700 active:bg-red-scale-200 disable:bg-gray-scale-50 disable:border-stroke-400 disable:text-red-scale-300',
   } as const;
 
   return (

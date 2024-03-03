2836c0acfa993e17e91f8f58254a561f2487e9ed
Im-younique <dyyim4725@gmail.com>
2024-03-03 20:30:41 +0900

    [chore] Divider 스타일 정의 변경

--- client/src/styles/styleConstant.ts
+++ client/src/styles/styleConstant.ts
@@ -5,40 +5,6 @@ export const positionStyle = {
   bottomLeft: 'bottom-0 left-0',
 };
 
-export const widthStyle = {
-  1: 'w-[1px]',
-  2: 'w-0.5',
-  4: 'w-1',
-  8: 'w-2',
-};
-
-export const heightStyle = {
-  1: 'h-px',
-  2: 'h-0.5',
-  4: 'h-1',
-  8: 'h-2',
-};
-
-export const mxStyle = {
-  0: 'mx-0',
-  1: 'mx-1',
-  3: 'mx-3',
-  4: 'mx-4',
-  6: 'mx-6',
-  8: 'mx-8',
-  16: 'mx-16',
-};
-
-export const myStyle = {
-  0: 'my-0',
-  1: 'my-1',
-  3: 'my-3',
-  4: 'my-4',
-  6: 'my-6',
-  8: 'my-8',
-  16: 'my-16',
-};
-
 export const avatarSizeStyle = {
   xxs: 'w-5 h-5',
   xs: 'w-7 h-7',

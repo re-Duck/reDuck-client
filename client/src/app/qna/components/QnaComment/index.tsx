45640c61c14e431c62ed5aa583a55558cddf488b
Im-younique <dyyim4725@gmail.com>
2024-03-11 23:35:13 +0900

    [Add] QnA 상세 UI 임시 커밋

---
+++ client/src/app/qna/components/QnaComment/index.tsx
@@ -0,0 +1,3 @@
+export default function QnaComment() {
+  return <div>QnaComment</div>;
+}
---
+++ client/src/app/qna/components/Toolbar/index.tsx
@@ -0,0 +1,7 @@
+export default function CommentToolbar() {
+  return (
+    <div className="flex">
+      <span>에디터 아이콘 공간</span>
+    </div>
+  );
+}
